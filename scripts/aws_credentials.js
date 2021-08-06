a609b8d48a059ce28dfa9aa0cef90cde23ab2973
Jack Southard <jacka@southard.net>
2021-08-06 16:12:14 -0700

    Add aws credentials

---
+++ scripts/aws_credentials.js
@@ -0,0 +1,8 @@
+function getCredentials() {
+    return {
+        region: "us-west-1",
+        endpoint: "https://dynamodb.us-west-1.amazonaws.com",
+        accessKeyId: "AKIAZZT2YHIZ2EPA3TNG",
+        secretAccessKey: "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"
+    };
+}
