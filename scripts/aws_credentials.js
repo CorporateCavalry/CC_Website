2cdee67b0aafa08e306aa18624fa3aee9b212812
Nathaniel Jew <nathankjew@gmail.com>
2021-08-16 04:36:19 -0700

    Remove outdated AWS Manager and AWS credentials

--- scripts/aws_credentials.js
+++
@@ -1,8 +0,0 @@
-function getCredentials() {
-    return {
-        region: "us-west-1",
-        endpoint: "https://dynamodb.us-west-1.amazonaws.com",
-        accessKeyId: "AKIAZZT2YHIZ2EPA3TNG",
-        secretAccessKey: "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"
-    };
-}
